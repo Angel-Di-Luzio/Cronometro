796089c3922912001c26f6ff9cd4bb97a5f9f31e
Angel David Garcez Di Luzio <angeldiluzio@gmail.com>
2025-08-09 17:56:34 -0400

    Incorporacion de logica

--- JS/main.js
+++ JS/main.js
@@ -11,16 +11,58 @@ let mili = 0;
 let seg = 0;
 let min = 0;
 let hrs = 0;
+let interID = null;
+let pausa = true;
 
 function update(){
 timer.innerHTML = `${hrs.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}:${seg.toString().padStart(2, "0")}.${mili.toString().padStart(2, "0")}`;
 }
 
-function crearItem(){
-    
+function estado(){
+    if (pausa){
+        btnI.innerHTML = "&#10074;&#10074;";
+        btnR.innerHTML = "Vuelta";
+        secuencia();
+        pausa = false;
+    }else{
+        btnI.innerHTML = "&#11208;";
+        btnR.innerHTML = "&#8634;";
+        secuencia();
+        pausa = true;
+    }
+}
+
+function secuencia(){
+    if (!interID){
+        interID = setInterval(() => {
+            mili++;
+            limite();
+            update();
+        },10)
+    }else if (interID){
+        clearInterval(interID);
+        interID = null;
+    }
+}
+
+function limite(){
+    if (mili > 99){
+        mili = 0;
+        seg++;
+    }else if (seg > 59){
+        min++;
+        seg = 0
+    }else if (min > 59){
+        hrs++;
+        min = 0;
+    }else if (hrs >= 24){
+        hrs = 24;
+        min = seg = mili = 0;
+    }
 }
 
 btnI.addEventListener("click", () => {
+    estado();
 });
 
 update();
