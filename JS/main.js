09505e354eb4abe1fc559aff172f2ad4d594a2c9
Angel David Garcez Di Luzio <angeldiluzio@gmail.com>
2025-08-10 16:30:31 -0400

    Finalizacion del Cronometro

--- JS/main.js
+++ JS/main.js
@@ -6,16 +6,30 @@ const btnR = document.getElementById("btn-r");
 const crp = document.getElementById("cuerpo");
 
 //Variables
-
 let mili = 0;
 let seg = 0;
 let min = 0;
 let hrs = 0;
+
+let textMili = "";
+let textSeg = "";
+let textMin = "";
+let textHrs = "";
+let v = 0;
+
+let tr, td1, td2;
+
 let interID = null;
 let pausa = true;
+let modo = true;
+//false = vueltas, true = reinicio
 
 function update(){
-timer.innerHTML = `${hrs.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}:${seg.toString().padStart(2, "0")}.${mili.toString().padStart(2, "0")}`;
+    textMili = mili.toString().padStart(2, "0");
+    textSeg = seg.toString().padStart(2, "0");
+    textMin = min.toString().padStart(2, "0");
+    textHrs = hrs.toString().padStart(2, "0");
+    timer.innerHTML = `${textHrs}:${textMin}:${textSeg}.${textMili}`;
 }
 
 function estado(){
@@ -24,11 +38,13 @@ function estado(){
         btnR.innerHTML = "Vuelta";
         secuencia();
         pausa = false;
+        modo = false;
     }else{
         btnI.innerHTML = "&#11208;";
         btnR.innerHTML = "&#8634;";
         secuencia();
         pausa = true;
+        modo = true;
     }
 }
 
@@ -61,8 +77,44 @@ function limite(){
     }
 }
 
+function reiniciar(){
+    if (mili > 0 || seg > 0 || min > 0 || hrs > 0){
+        mili = seg = min = hrs = 0;
+        update();
+    }
+
+    crp.innerHTML = "";
+    v = 0;
+
+}
+
+function vueltas(){
+    v++;
+    const vlt = v;
+    const tmp = `${textHrs}:${textMin}:${textSeg}.${textMili}`;
+
+    tr = document.createElement("tr");
+    crp.appendChild(tr);
+    td1 = document.createElement("td");
+    td2 = document.createElement("td");
+
+    td1.innerHTML = vlt;
+    td2.innerHTML = tmp;
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+}
+
 btnI.addEventListener("click", () => {
     estado();
 });
-
+//false = vueltas, true = reinicio
+btnR.addEventListener("click", () => {
+    if (modo){
+        reiniciar();
+    }else{
+        vueltas();
+    }
+})
+    
 update();
